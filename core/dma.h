ab4caf348d3ebfddd380375e3d1b37696383aefe
Benjamin Gutman <benjamingutman@icloud.com>
2026-08-30 18:18:39 -0400

    Added enums and fixed naming consistency with other files

--- core/dma.h
+++ core/dma.h
@@ -9,32 +9,56 @@ extern "C" {
 #endif
 
 
-void dma_init(DMA_TypeDef *dma, uint8_t stream);
+typedef enum {
+	DMA_PERIPHERY_TO_MEMORY,
+	DMA_MEMORY_TO_PERIPHERY,
+	DMA_MEMORY_TO_MEMORY
+} DMA_Direction;
 
-void dma_set_channel(DMA_TypeDef *dma, uint8_t stream, uint8_t channel);
+typedef enum {
+	DMA_DATA_8BIT,
+	DMA_DATA_16BIT,
+	DMA_DATA_32BIT
+} DMA_DataSize;
 
-void dma_set_address(DMA_TypeDef *dma, uint8_t stream, uint32_t peripheral_address, uint32_t memory_address);
+typedef enum {
+	DMA_PRIORITY_LOW,
+	DMA_PRIORITY_MEDIUM,
+	DMA_PRIORITY_HIGH,
+	DMA_PRIORITY_VERY_HIGH
+} DMA_Priority;
 
-void dma_set_transfer_length(DMA_TypeDef *dma, uint8_t stream, uint16_t length);
+typedef enum {
+	DMA_SINGLE,
+	DMA_INCREMENTAL_BURST
+} DMA_Burst;
 
-void dma_set_direction(DMA_TypeDef *dma, uint8_t stream, uint8_t direction);
+void dma_init(DMA_TypeDef* dma, uint8_t stream);
 
-void dma_set_data_size(DMA_TypeDef *dma, uint8_t stream, uint8_t peripheral_size, uint8_t memory_size);
+void dma_set_channel(DMA_TypeDef* dma, uint8_t stream, uint8_t channel);
 
-void dma_enable_memory_increment(DMA_TypeDef *dma, uint8_t stream);
+void dma_set_address(DMA_TypeDef* dma, uint8_t stream, uint32_t peripheral_address, uint32_t memory_address);
 
-void dma_enable_peripheral_increment(DMA_TypeDef *dma, uint8_t stream);
+void dma_set_transfer_length(DMA_TypeDef* dma, uint8_t stream, uint16_t length);
 
-void dma_enable_circular_mode(DMA_TypeDef *dma, uint8_t stream);
+void dma_set_direction(DMA_TypeDef* dma, uint8_t stream, DMA_Direction direction);
 
-void dma_set_priority(DMA_TypeDef *dma, uint8_t stream, uint8_t priority);
+void dma_set_data_size(DMA_TypeDef* dma, uint8_t stream, DMA_DataSize peripheral_size, DMA_DataSize size);
+
+void dma_enable_memory_increment(DMA_TypeDef* dma, uint8_t stream);
+
+void dma_enable_peripheral_increment(DMA_TypeDef* dma, uint8_t stream);
+
+void dma_enable_circular_mode(DMA_TypeDef* dma, uint8_t stream);
+
+void dma_set_priority(DMA_TypeDef* dma, uint8_t stream, DMA_Priority priority);
 
 
-void dma_enable(DMA_TypeDef *dma, uint8_t stream);
+void dma_enable(DMA_TypeDef* dma, uint8_t stream);
 
-void dma_disable(DMA_TypeDef *dma, uint8_t stream);
+void dma_disable(DMA_TypeDef* dma, uint8_t stream);
 
-uint8_t dma_is_enabled(DMA_TypeDef *dma, uint8_t stream);
+uint8_t dma_is_enabled(DMA_TypeDef* dma, uint8_t stream);
 
 
 #ifdef __cplusplus
